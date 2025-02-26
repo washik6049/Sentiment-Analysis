bc0e5af25df4d3fb302a3faad5dd0edf16991045
washik faieaz <washikfaieaz@gmail.com>
2025-02-27 04:30:37 +0600

    update README, remove Vercel configuration, and adjust static file settings

--- sentiment_analyzer/settings.py
+++ sentiment_analyzer/settings.py
@@ -116,9 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# Static files (optional, but good practice)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
